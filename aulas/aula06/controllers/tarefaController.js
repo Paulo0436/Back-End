64f0fff3d168338d318dc3922a1395216ddb653f
Paulo <p.souz436@gmail.com>
2025-09-23 21:37:35 -0300

    Finalizando aula06

--- aulas/aula06/controllers/tarefaController.js
+++ aulas/aula06/controllers/tarefaController.js
@@ -1,13 +1,46 @@
-const tarefas = [];
+const model = require("../models/tarefaModel")
+
 const listarTarefas  = (req , res ) => {
-    res.json(tarefas)
+    res.json(model.listar())
 };
 
 const criarTarefa = (req , res ) => {
-    const novaTarefa = { ... req.body, id : tarefas.length + 1};
-    tarefas.push(novaTarefa)
+    const novaTarefa = model.criar(req.body);
     res.status(201).json(novaTarefa)
 }
 
+const buscarTarefa = (req,res,next) =>{
+    const { id } = req.params;
+    const tarefaEncontrada = model.obter(id);
+    if(tarefaEncontrada){
+        req.tarefa = tarefaEncontrada;
+        return next();
+    } 
+    res.status(404).json({ msg: "Tarefa não encontrada"});
+}
 
-module.exports = { listarTarefas , criarTarefa };
+const obterTarefa = (req ,res ) => {
+    const { id } = req.params;
+    const tarefaEncontrada = model.obter(id);
+    res.json(tarefaEncontrada);
+}
+
+const atualizarTarefa = (req,res) =>{
+    const { id } = req.params;
+    const tarefaEncontrada = model.atualizar({ id , ...req.body});
+     res.json(tarefaEncontrada);
+}
+
+
+
+const removerTarefa = (req,res) => {
+    const { id } = req.params;
+     model.remover(id);
+     res.status(204).end();
+    
+   
+
+}
+
+
+module.exports = { listarTarefas , criarTarefa ,buscarTarefa, obterTarefa , atualizarTarefa , removerTarefa };
